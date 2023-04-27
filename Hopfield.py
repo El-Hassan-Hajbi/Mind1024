d71534654a23153ad34f00c1c56e1f9805af81ec
El Hassan_ <66976572+El-Hassan-Hajbi@users.noreply.github.com>
2023-04-27 21:21:15 +0200

    Add files via upload

--- Hopfield.py
+++ Hopfield.py
@@ -1,67 +1,74 @@
-import numpy as np
+#!/usr/bin/env python3
+# Setup
+import matplotlib.pyplot as plt
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from itertools import combinations, product
+from tqdm import tqdm # progress bar
 
-class HopfieldNetwork :
-    def __init__(self, numberNeurons) :
-        self.INTERVAL = 20
-        self.ACTIVATION_THRESHOLD = 0
-        self.CONVERGE_NODES = 5
-        self.num_neurons = numberNeurons
-        self._weights = []
-        self._values = []
-        for i in range(self.numberNeurons):
-            self._weights[i] = []
-            self._values[i] = -1
-        for j in range(self.numberNeurons):
-            self._weights[i][j] = 1
+# import data
+allTrain = pd.read_csv( './datapy/train.csv' )
+n = len( allTrain )
+X_train = allTrain.drop( 'label', 1).to_numpy()
+print(np.shape(X_train))
+Y_train = allTrain[ 'label' ].to_numpy()
+del allTrain
+test = pd.read_csv( './datapy/test.csv' )
+X_test = test.to_numpy()
+print( "data import complete" )
+
+
+class HopfieldNetwork( object ):
+    def __init__( self, pattern, rule='pseudo-inverse' ):
+        '''expects patterns to have values belonging to {-1, 1}
+           initializes with Hebbian rule'''
+        self.n = pattern[0].size
+        self.order = np.arange( self.n )
         
-    
-    def train(self, samples) :
-        self._resetWeights()
-        assert(self.num_neurons == len(self._values))
-        for i in range(self.numberNeurons):
-            for j in range(self.numberNeurons):
-                for k in range(len(samples)) :
-                    if samples[k][i] == samples[k][j] :
-                        self._weights[i][j] += 1 / len(samples)
-                    else :
-                        self._weights[i][j] -= 1 / len(samples)
-                    
-    def _resetWeights(self) :
-        assert(self.num_neurons == len(self._values))
-        for i in range(self.numberNeurons):
-            for j in range(self.numberNeurons):
-                self._weights[i][j] = 0
-      
-    def convergeStep(self) :
-        for x in range(self.CONVERGE_NODES) :
-            self._convergeRandomNode()
-    
-    def _convergeRandomNode(self) :
-        nodeIndex = np.floor(np.random() * self._values.length)
-        sum = 0
-        for j in range(len(self._values)):
-            if j == nodeIndex:
-                continue
-       
-        w = self._weights[nodeIndex][j]
-        v = self._values[j]
-        sum += w * v
-        
-        if sum > self.ACTIVATION_THRESHOLD :
-            self._values[nodeIndex] = 1
-        else :
-            self._values[nodeIndex] = -1
-        
+        if rule == 'hebbian':
+            self.w = np.tensordot( pattern, pattern, axes=( ( 0 ), ( 0 ) ) ) / len( pattern )
+            self.w[ self.order, self.order ] = 0.0
+        elif rule == 'pseudo-inverse':
+            c = np.tensordot( pattern, pattern, axes=( ( 1 ), ( 1 ) ) ) / len( pattern )
+            cinv = np.linalg.inv( c )
+            self.w = np.zeros( ( self.n, self.n ) )
+            for k, l in product( range( len( pattern ) ), range( len( pattern ) ) ):
+                self.w = self.w + cinv[ k, l ] * pattern[ k ] * pattern[ l ].reshape( ( -1, 1 ) )
+            self.w = self.w / len( pattern )
+        else:
+            assert false, 'invalid learning rule: {}\nplease choose hebbian or pseudo-inverse'.format( rule )
 
-    def setVector(self, values) :
-        for i in range(len(self._values)):
-            self._values[i] = values[i]
-    
-    def vector(self) :
-        return self._values.slice()
+    def processBatch( self, x, iters=4 ):
+        '''input should be same size format as patterns. Implements asynchronous update'''
+        h = np.array( x, dtype=float )
+        for _ in range( iters ):
+            np.random.shuffle( self.order )
+            for i in self.order:
+                h[ :, i ] = np.tensordot( self.w[ i ], h, axes=( ( -1 ), ( -1 ) ) )
+                h[ :, i ] = np.where( h[ :, i ] < 0, -1.0, 1.0 )
+        return h
 
+# create and train hopfield networks (one for each pair, because expressiveness sucketh)
 
+#preprocess samples
+supervised = True
+iters = 8
 
-network = HopfieldNetwork()
+factor = 2.0 / np.max( X_train )
+avg_classes = lambda: np.array( [ factor * np.mean( X_train[ Y_train == l, : ], axis=0 ) - 1.0
+                                  for l in np.unique( Y_train ) ] )
+patterns = avg_classes() if supervised else ( factor * X_train - 1 )
+threshold = np.zeros( iters )
+bitPatterns = np.where( patterns < threshold[ -1 ], -1, 1 )
+XB_train = np.where( ( factor * X_train - 1 ) < threshold[ -1 ], -1, 1 ) if supervised else bitPatterns
 
-network.train(sample)
+hopnet = HopfieldNetwork( bitPatterns )
+XH_train = hopnet.processBatch( XB_train )
+print((XH_train))
+print((XB_train))
+# peek results; run again to see a new selection
+# processed image
+
+plt.imshow( XH_train[ 5 ].reshape( ( 28, 28 ) ), cmap='gray' )
--- main.js
+++ main.js
@@ -1,13 +1,248 @@
 document.addEventListener('DOMContentLoaded', function() {
 (function() {
-    var mnist = require('mnist');
+  function f(vector) {
+    var res = [];
+    for (var element of vector) {
+      if (element == 0) {
+          res[res.length] = -1;
+      }
+      else {
+        res[res.length] = 1;
+      }
+    }
+    return res;
+  }
+
+  function prepare_data(dataset){
+    var res= [];
+    for (var i = 0; i< dataset.length; i++) {
+      res[i] = [];
+      var ith = dataset[i].input;
+      for (var j = 0; j< ith.length; j++) {
+        if (ith[j] == 0) {res[i][j] = -1;}
+        else {res[i][j] = 1;}
+      }
+    }
+    return res;
+  }
+  
+  const jsonFiles = [
+    '../digits/0.json',
+    '../digits/1.json',
+    '../digits/2.json',
+    '../digits/3.json',
+    '../digits/4.json',
+    '../digits/5.json',
+    '../digits/6.json',
+    '../digits/7.json',
+    '../digits/8.json',
+    '../digits/9.json',
+  ];
+  
+  // MNIST digits
+  var MNIST = [];
+  var raw = [];
+  // size of the sample images (28 x 28)
+  var size = 28;
+  
+  async function loadJsonFiles() {
+    for (const file of jsonFiles) {
+      const response = await fetch(file);
+      const json = await response.json();
+      raw.push(json.data);
+    }
+  }
+  
+  loadJsonFiles().then(() => {
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function (id) {
+      // mnist digit
+      var digit = {
+        id: id
+      };
+    
+      // raw data
+      digit.raw = raw[digit.id];
+    
+      // number of samples
+      digit.length = digit.raw.length / (size * size) | 0;
+    
+      // get one sample
+      digit.get = function (_which) {
+        var which = _which;
+        // if not specified, or if invalid, pick a random sample
+        if ('undefined' == typeof which || which > digit.length || which < 0) {
+          which = Math.random() * digit.length | 0;
+        }
+    
+        // generate sample
+        var sample = [];
+        for (
+          var length = size * size,
+          start = which * length,
+          i = 0;
+          i < length;
+          sample.push(digit.raw[start + i++])
+        );
+        return sample;
+      }
+    
+      // get a range of samples
+      digit.range = function (start, end) {
+        if (start < 0)
+          start = 0;
+        if (end >= digit.length)
+          end = digit.length - 1;
+        if (start > end) {
+          var tmp = start;
+          start = end;
+          end = tmp;
+        }
+        var range = [];
+        for (
+          var i = start;
+          i <= end;
+          range.push(digit.get(i++))
+        );
+        return range;
+      }
+    
+      // get set of digits, ready to be used for training or testing
+      digit.set = function (start, end) {
+        var set = [];
+        var output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        output[digit.id] = 1;
+        var range = digit.range(start, end);
+        for (
+          var i = 0;
+          i < range.length;
+          set.push({
+            input: range[i++],
+            output: output
+          })
+        );
+        return set;
+      }
+    
+      // add mnist digit
+      MNIST.push(digit);
+    });
+    
+    // Generates non-overlaping training and a test sets, with the desired ammount of samples
+    MNIST.get = function (count) {
+      var range = [];
+      for (var i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        range = range.concat(this[i].set(0, this[i].length));
+      }
+      range = shuffle(range);
+      if (Number(count)) {
+        range = range.slice(0, Number(count));
+      }
+      return range;
+    }
+    
+    
+    // Generates non-overlaping training and a test sets, with the desired ammount of samples
+    MNIST.set = function (_training, _test) {
+      var training = _training / 10 | 0;
+      var test = _test / 10 | 0;
+    
+      if (training < 1)
+        training = 1;
+      if (test < 1)
+        test = 1;
+    
+      // check that there are enough samples to make the sets, and change the ammounts if they are too big
+      if (training + test + 1 > MNIST.__MINLENGTH) {
+        console.warn('There are not enough samples to make a training set of ' + training + ' elements and a test set of ' + test + ' elements.');
+        if (training > test) {
+          test = MNIST.__MINLENGTH * (test / training);
+          training = MNIST.__MINLENGTH - training;
+        }
+        else {
+          training = MNIST.__MINLENGTH * (training / test);
+          test = MNIST.__MINLENGTH - test;
+        }
+      }
+    
+      // make both sets
+      var trainingSet = [];
+      var testSet = [];
+    
+      for (var i = 0; i < 10; i++) {
+        trainingSet = trainingSet.concat(MNIST[i].set(0, training - 1));
+        testSet = testSet.concat(MNIST[i].set(training, training + test - 1));
+      }
+    
+      // return the sets, shuffled
+      return {
+        training: shuffle(trainingSet),
+        test: shuffle(testSet)
+      }
+    }
+    
+    // draws a given digit in a canvas context
+    MNIST.draw = function (digit, context, offsetX, offsetY) {
+      var imageData = context.getImageData(offsetX || 0, offsetY || 0, size, size);
+      for (var i = 0; i < digit.length; i++) {
+        imageData.data[i * 4] = digit[i] * 255;
+        imageData.data[i * 4 + 1] = digit[i] * 255;
+        imageData.data[i * 4 + 2] = digit[i] * 255;
+        imageData.data[i * 4 + 3] = 255;
+      }
+      context.putImageData(imageData, offsetX || 0, offsetY || 0);
+    }
+    
+    // takes an array of 10 digits representing a number from 0 to 9 (ie. any output in a dataset) and returns the actual number
+    MNIST.toNumber = function (array) {
+      return array.indexOf(Math.max.apply(Math, array));
+    }
+    
+    // CommonJS & AMD
+    if (typeof define !== 'undefined' && define.amd) {
+      define([], function () { return MNIST });
+    }
+    
+    // Node.js
+    if (typeof module !== 'undefined' && module.exports) {
+      module.exports = MNIST;
+    }
+    
+    // Browser
+    if (typeof window == 'object') {
+      (function () {
+        var old = window['mnist'];
+        MNIST.ninja = function () {
+          window['mnist'] = old;
+          return MNIST;
+        };
+      })();
+    
+      window['mnist'] = MNIST;
+    }
+    
+    //+ Jonas Raoni Soares Silva
+    //@ http://jsfromhell.com/array/shuffle [rev. #1]
+    
+    function shuffle(v) {
+      for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+      return v;
+    };
+  
+    
+    var set = MNIST.set(8000, 2000);
+  
+    var trainingSet = set.training;
+    var digit = MNIST[9].get();
+    
+    //var mnist = require('mnist');
     var DRAWING_COUNT = 1;
-    var PICTURE_SIZE = 26;
+    var PICTURE_SIZE = 28;
     var CANVAS_SIZE = 300;
     var INTERVAL = 20;
     var ACTIVATION_THRESHOLD = 0;
     var CONVERGE_NODES = 5;
-
+  
+  
   function Hopfield(nodeCount) {
     this._weights = [];
     this._values = [];
@@ -21,7 +256,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   }
-
+  
   Hopfield.prototype.train = function(samples) {
     this._resetWeights();
     this.patterns[this.patterns.length] = samples;
@@ -40,7 +275,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   Hopfield.prototype.recall = function(inputPattern) {
     let outputPattern = new Array(this.numNeurons).fill(0);
-
+  
     for (let i = 0; i < this.numNeurons; i++) {
       let activation = 0;
       for (let j = 0; j < this.numNeurons; j++) {
@@ -48,7 +283,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       outputPattern[i] = activation >= 0 ? 1 : -1;
     }
-
+  
     return outputPattern;
   }
   Hopfield.prototype._resetWeights = function() {
@@ -59,11 +294,12 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   };
-
+  
   Hopfield.prototype.classify = function(pattern) {
     // Apply the pattern to the Hopfield network to retrieve a stable state
+    //pattern = image and a onehotencoded value 
     const stableState = this.recall(pattern);
-
+  
     // Check which pattern the stable state matches
     for (let i = 0; i < this.patterns.length; i++) {
       const currentPattern = this.patterns[i];
@@ -71,20 +307,20 @@ document.addEventListener('DOMContentLoaded', function() {
         return `The input pattern matches pattern ${i + 1}`;
       }
     }
-
+  
     // If no pattern matches, return an error message
     return "Error: the input pattern does not match any of the trained patterns";
   }
-
-// Helper function to compare two arrays element by element
-function arraysEqual(arr1, arr2) {
+  
+  // Helper function to compare two arrays element by element
+  function arraysEqual(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
   }
   return true;
-}
-
+  }
+  
     function Canvas() {
       this._cells = [];
       for (var i = 0; i < PICTURE_SIZE*PICTURE_SIZE; ++i) {
@@ -101,11 +337,11 @@ function arraysEqual(arr1, arr2) {
     Canvas.prototype.element = function() {
       return this._element;
     };
-
+  
     Canvas.prototype.print_cells = function() {
         return this._cells;      
     }
-
+  
     Canvas.prototype.vector = function() {
       var res = [];
       for (var i = 0, len = this._cells.length; i < len; ++i) {
@@ -142,9 +378,9 @@ function arraysEqual(arr1, arr2) {
           var rectLeft = x*tileSize;
           var rectTop = y*tileSize;
           if (this._cells[x+y*PICTURE_SIZE]) {
-            ctx.fillStyle = 'black';
-          } else {
             ctx.fillStyle = 'white';
+          } else {
+            ctx.fillStyle = 'black';
           }
           ctx.fillRect(rectLeft, rectTop, tileSize, tileSize);
         }
@@ -193,26 +429,46 @@ function arraysEqual(arr1, arr2) {
       }.bind(this));
     };
     function App() {
-        this._drawingsElement = document.getElementById('drawings');
-        var drawing = new Canvas();
-        this.drawing = drawing;
-        this._drawingsElement.appendChild(drawing.element());
-        var boutonPrintCell = document.getElementById('print-cells');
-        var output_text = document.getElementById('output-classification');
-        boutonPrintCell.addEventListener('click', function(){
-          console.log(drawing.print_cells());
+      var set = MNIST.set(1, 1);
+  
+      var trainingSet = set.training;
+      var testSet = set.test;
+      
+      //console.log(prepare_data(trainingSet));
+      var training_data = prepare_data(trainingSet);
+      var test_data = prepare_data(testSet);
+      //console.log(test_data);
+      var digit = MNIST[5].get();
+      this._drawingsElement = document.getElementById('drawings');
+      var drawing = new Canvas();
+      drawing.setVector(f(digit));
+      this.drawing = drawing;
+      this._drawingsElement.appendChild(drawing.element());
+      var boutonPrintCell = document.getElementById('print-cells');
+      var boutonClassification = document.getElementById('run-classification');
+      var output_text = document.getElementById('output-classification');
+      boutonPrintCell.addEventListener('click', function(){
+        console.log(f(drawing.print_cells()));
+      });
+      boutonClassification.addEventListener('click', function(){
+        console.log(network.classify(f(drawing.print_cells())));
       });
       var network = new Hopfield(1024);
-      network.train([false, false, false, false, false, false, true, true, false, false, false, true, true, false, false, false, false, true, false, false, false, false, true, false, false]);
-      console.log(network.patterns);
+      network.train(training_data);
+      //console.log(network.patterns);
       //console.log(network.classify(drawing.print_cells()));
-      console.log(network.classify([false, false, false, false, false, false, true, true, false, false, false, true, true, false, false, false, false, true, false, false, false, false, true, false, false]));
+      //console.log(network.classify(test_data[0]));
       }    
     
   
-    window.addEventListener('load', function() {
-        new App();
+      new App();
+  
+    
+    //console.log(f(digit));
+  });
+  
 
-    });
+
+
   
   })();});
